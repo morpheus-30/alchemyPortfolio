971335cf824b918e08edbdeaa4dbca4596b328e1
morpheus-30 <21001003083.jcboseust.ac.in>
2023-09-13 02:50:51 +0530

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/alchemy/',
   plugins: [react()],
 })
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import viteCompression from 'vite-plugin-compression';
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command, mode }) => {
+//   return {
+//     plugins: [
+//       react(),
+//       viteCompression()
+//     ],
+//     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+//     base: './'
+//   }
+// });
